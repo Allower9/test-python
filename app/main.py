6fa85eb249e4f22b36ab0878afd02445f80b03f2
Allower <150520849+Allower9@users.noreply.github.com>
2025-06-25 00:18:55 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,35 +1,7 @@
-# app/main.py
 from fastapi import FastAPI
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
 
 app = FastAPI()
 
-# Подключение к БД
-DATABASE_URL = "postgresql://myappuser:secretpassword@192.168.1.126/myappdb"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
-
-# Модель таблицы
-class Item(Base):
-    __tablename__ = "items"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-
-Base.metadata.create_all(bind=engine)
-
 @app.get("/")
 def read_root():
-    db = SessionLocal()
-    items = db.query(Item).all()
-    return {"items": [item.name for item in items]}
-
-@app.post("/add/{name}")
-def add_item(name: str):
-    db = SessionLocal()
-    item = Item(name=name)
-    db.add(item)
-    db.commit()
-    return {"message": f"Added {name}"}
-
+    return {"message": "Привет из FastAPI!"}
