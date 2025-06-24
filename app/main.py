b177330f4b0835b4406aa2e47f9d4cf16e444b02
allower9 <lesha18092005@mail.ru>
2025-06-24 22:01:38 +0300

    2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
+import asyncpg
 from fastapi import FastAPI
 
 app = FastAPI()
 
+DATABASE_URL = "postgresql://myappuser:secretpassword@192.168.1.126:5432/myappdb"
+
+@app.on_event("startup")
+async def startup():
+    app.state.db = await asyncpg.connect(DATABASE_URL)
+
+@app.on_event("shutdown")
+async def shutdown():
+    await app.state.db.close()
+
 @app.get("/")
-def read_root():
-    return {"message": "Привет из FastAPI!"}
+async def read_root():
+    row = await app.state.db.fetchrow("SELECT 'Hello from PostgreSQL!' AS message;")
+    return {"message": row["message"]}
 
